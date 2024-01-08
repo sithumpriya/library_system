38f0a0e7cf4816b382e5a45494f4b09909f524ea
Sakuntha Prabodha <sakunthaprabodha@gmail.com>
2024-01-08 21:23:54 +0530

    Navigation bar added

--- index.php
+++ index.php
@@ -13,10 +13,16 @@ if(empty($_SESSION["user_id"])){
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- External CSS file -->
+    <link rel="stylesheet" type="text/css" href="index.css">
 
     <title>Library System</title>
 </head>
 <body>
-
+    <!-- Navigation bar -->
+    <div class="navbar">
+        <span style="cursor: pointer;" onclick="window.location.href='index.php'">Library System</span>
+    </div>
 </body>
 </html>
