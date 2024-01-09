b3c17748a9e5ee6a6151e76035be231471588fac
Sakuntha Prabodha <sakunthaprabodha@gmail.com>
2024-01-09 10:13:15 +0530

    Implemented Save and Cancel buttons with functionality to show or hide the buttons

--- Feature login reg/user.php
+++ Feature login reg/user.php
@@ -43,54 +43,71 @@ if(empty($_SESSION["user_id"])){
             <form action="" method="post" autocomplete="off">
                 <div class="row">
                     <div class="col-25">
-                    <label for="firstName">First Name</label>
+                        <label for="firstName">First Name</label>
                     </div>
                     <div class="col-75">
-                    <input type="text" id="firstName" name="firstName" value="<?php echo $row["first_name"]; ?>" placeholder="Enter first name..">
+                        <input type="text" id="firstName" name="firstName" value="<?php echo $row["first_name"]; ?>" onkeydown="funDisplay()" placeholder="Enter first name..">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-25">
-                    <label for="lastName">Last Name</label>
+                        <label for="lastName">Last Name</label>
                     </div>
                     <div class="col-75">
-                    <input type="text" id="lastName" name="lastName" value="<?php echo $row["last_name"]; ?>" placeholder="Enter last name..">
+                        <input type="text" id="lastName" name="lastName" value="<?php echo $row["last_name"]; ?>" onkeydown="funDisplay()" placeholder="Enter last name..">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-25">
-                    <label for="userId">User Id</label>
+                        <label for="userId">User Id</label>
                     </div>
                     <div class="col-75">
-                    <input type="text" id="userId" name="userId" value="<?php echo $row["user_id"]; ?>" pattern="U[0-9]{3}" placeholder="Enter user id..(UXXX)" required>
+                        <input type="text" id="userId" name="userId" value="<?php echo $row["user_id"]; ?>" pattern="U[0-9]{3}" onkeydown="funDisplay()" placeholder="Enter user id..(UXXX)" required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-25">
-                    <label for="username">Username</label>
+                        <label for="username">Username</label>
                     </div>
                     <div class="col-75">
-                    <input type="text" id="username" name="username" value="<?php echo $row["username"]; ?>" placeholder="Enter username.." required>
+                        <input type="text" id="username" name="username" value="<?php echo $row["username"]; ?>" onkeydown="funDisplay()" placeholder="Enter username.." required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-25">
-                    <label for="email">Email</label>
+                        <label for="email">Email</label>
                     </div>
                     <div class="col-75">
-                    <input type="email" id="email" name="email" value="<?php echo $row["email"]; ?>" placeholder="Enter email address..">
+                        <input type="email" id="email" name="email" value="<?php echo $row["email"]; ?>" onkeydown="funDisplay()" placeholder="Enter email address..">
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-25">
-                    <label for="password">Password</label>
+                        <label for="password">Password</label>
                     </div>
                     <div class="col-75">
-                    <input type="text" id="password" name="password" value="<?php echo $row["password"]; ?>" onclick="this.value=''" placeholder="Set a new password..(optional)">
+                        <input type="text" id="password" name="password" value="<?php echo $row["password"]; ?>" onclick="this.value=''" onfocus="funDisplay()" placeholder="Set a new password..(optional)">
                     </div>
+                </div>
+                <!-- Buttons for save and cancel the changes -->
+                <div class="row" id="button" style="display: none;">
+                    <input type="submit" value="Save" name="update">
+                    <input type="reset" value="Cancel" onclick="funHide()">
                 </div>
             </form>
         </div>
     </div>
+
+    <script>
+        // When the user onkeydown display save and cancel buttons
+        function funDisplay() {
+            document.getElementById("button").style.display = "block";
+        }
+
+        // When the user clicks cancel button hide save and cancel buttons
+        function funHide() {
+            document.getElementById("button").style.display = "none";
+        }
+    </script>
 </body>
 </html>
