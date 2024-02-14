a78887c1cbb66bf45656dea097c2417308f159c5
chamodavishka <chamodavishka2021@gmail.com>
2024-02-14 15:26:59 +0530

    Feature Book Registration Updated!

--- Feature books reg/books_process.php
+++ Feature books reg/books_process.php
@@ -8,7 +8,12 @@ if (isset($_POST['update_book']))
     $bookID	= mysqli_real_escape_string($conn, $_POST['bookID']);
     $bookID_old	= mysqli_real_escape_string($conn, $_POST['bookID_old']);
     $bookName	= mysqli_real_escape_string($conn, $_POST['bookName']);
-    $categories  = mysqli_real_escape_string($conn, $_POST['category_id']);
+    $categories  = mysqli_real_escape_string($conn, $_POST['category_name']);
+
+    $sql = "SELECT category_id FROM bookcategory WHERE category_Name = '$categories'";
+    $cate_id = $conn->query($sql);
+    $cate_id = mysqli_fetch_assoc($cate_id);
+    $category_id = $cate_id['category_id'];
 
     $duplicate = mysqli_query($conn, "SELECT book_id FROM book WHERE book_id = ANY(SELECT book_id FROM book WHERE book_id = '$bookID' AND book_id != '$bookID_old')");
 
@@ -18,9 +23,9 @@ if (isset($_POST['update_book']))
         exit(0);
     } else {
         if($bookID == $bookID_old) {
-            $query = "UPDATE book SET book_id='$bookID', book_name='$bookName', category_id='$categories' WHERE book_id='$bookID'";
+            $query = "UPDATE book SET book_id='$bookID', book_name='$bookName', category_id='$category_id' WHERE book_id='$bookID'";
         } else {
-            $query = "UPDATE book SET book_id='$bookID', book_name='$bookName', category_id='$categories' WHERE book_id='$bookID_old'";
+            $query = "UPDATE book SET book_id='$bookID', book_name='$bookName', category_id='$category_id' WHERE book_id='$bookID_old'";
         }
     
         $query_run = mysqli_query($conn, $query);
